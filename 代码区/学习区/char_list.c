155da7d5a173bc2f92a94fa6544a589308dbfafb
Muxxs <muxxs@foxmail.com>
2020-03-07 01:12:32 +0800

    字符数组

---
+++ 代码区/学习区/char_list.c
@@ -0,0 +1,28 @@
+/*
+ * @Email: muxxs@foxmail.com
+ * @Auther: Muxxs
+ */
+/*
+ * @Editors: CNU 19 Muxxs
+ * @Email: muxxs@foxmail.com
+ */
+
+#include<stdio.h>
+
+int main(){
+    char a[10];
+    for (int i = 0; i < 10; i++)
+    {
+        /* code */
+        scanf("%c",&a[i]);
+    }
+
+    // 为什么只有五个？？？？？
+
+    for (int i = 0; i < 10; i++)
+    {
+        /* code */
+        printf("%c",a[i]);
+    }
+    return 0;
+}
