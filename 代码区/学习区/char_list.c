27e8bcfb778332841cbf5814505fd56ffced9800
Muxxs <muxxs@foxmail.com>
2020-03-07 12:00:20 +0800

    char_list update

--- 代码区/学习区/char_list.c
+++ 代码区/学习区/char_list.c
@@ -6,7 +6,7 @@
 
 #include<stdio.h>
 
-int main(){
+int list1(){
     char a[10];
     for (int i = 0; i < 10; i++)
     {
@@ -14,12 +14,25 @@ int main(){
         scanf("%c",&a[i]);
     }
 
-    // 回车也会作为一个字符存下（ASCII码为10）
+    // 回车也会作为一个字符存下（ASCII码为10） /0为空字符
 
     for (int i = 0; i < 10; i++)
     {
         /* code */
         printf("%d:%d\n",i,a[i]);
     }
+    return 0;
+}
+
+int list2(){
+    char c[]={"I Love China"};
+    puts(c);  //->直接输出字符串
+    printf("%s\n",c);
+    printf("%d\n",c[12]); //->最后一个是0 ACSII中是空字符所以不打印
+    return 0;
+}
+
+int main(){
+    list2();
     return 0;
 }
